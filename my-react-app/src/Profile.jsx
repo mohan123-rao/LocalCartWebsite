ae41adde89ff55b356d75bc99adb2fe79070ba37
mohan123-rao <mohanaraopeddinti080@gmail.com>
2026-02-18 18:24:31 +0530

    Update Profile.jsx

--- my-react-app/src/Profile.jsx
+++ my-react-app/src/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import EditItem from "./Edititem";
+import EditItem from "./EditItem";
 import { useNavigate } from "react-router-dom";
 const baseURL = import.meta.env.VITE_API_URL;
 
